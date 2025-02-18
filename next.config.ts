cb4f18aaa1aba4334ca64a74cb430ac40fa05729
tolak.o <tolak.o@outlook.com>
2025-02-18 13:14:40 +0100

    setup images for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/personal-page', // GitHub repo name
+  basePath: process.env.NODE_ENV === 'production' ? '/personal-page' : '', // GitHub repo name
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-page' : '',
   images: {
     unoptimized: true, // Required because GitHub Pages doesn't support Next.js image optimization
   },
